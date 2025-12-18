e89d703a598eb5431a10c0d847a0751745ba2be1
bala-amypo <bala@amypo.in>
2025-12-18 09:08:49 +0000

    User commit

--- src/main/java/com/example/demo/exception /globalExceptionalHandler.java
+++ src/main/java/com/example/demo/exception /globalExceptionalHandler.java
@@ -6,7 +6,11 @@ public class globalExceptionHandler
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?>handleFieldError(MethodArgumentNotValidException ex)
     {
+    Map<string,String>error= new HashMap<>();
+    ex.getBindingResult()
+    .getFieldErrors().forEach(err-> error.put(err.getField(),err.getDefaultMessage()));
     
-    }
-    
+    return new RessponseEntity<>(error,HttpStatus.BAD_REQUEST);
+        }
+    @ExceptionalH
 }
