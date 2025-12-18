ddb30e5a20f92ce2f64078fef85e241beb0003a7
bala-amypo <bala@amypo.in>
2025-12-18 06:32:39 +0000

    User commit

--- src/main/java/com/example/demo/exception /globalExceptionalHandler.java
+++ src/main/java/com/example/demo/exception /globalExceptionalHandler.java
@@ -1,12 +1,12 @@
 package com.example.demo.exception;
-import 
+ 
 @RestControllerAdvice
 public class globalExceptionHandler
 {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?>handleFieldError(MethodArgumentNotValidException ex)
     {
-        Map<String,String>
+    
     }
     
 }
